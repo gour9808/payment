1fff1216f4ea31eaede2dde0101d7b6a9e742a5e
gour9808 <gouravsharma614@gmail.com>
2022-02-26 01:34:57 +0530

    added new comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaymentComponent } from './payment/payment.component';
+import { PaymentHistoryComponent } from './payment-history/payment-history.component';
+import { PaymentHistoryDetailsComponent } from './payment-history-details/payment-history-details.component';
+import { PaymentContainerComponent } from './payment-container/payment-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaymentComponent
+    PaymentComponent,
+    PaymentHistoryComponent,
+    PaymentHistoryDetailsComponent,
+    PaymentContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/payment-container/payment-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-container',
+  templateUrl: './payment-container.component.html',
+  styleUrls: ['./payment-container.component.scss']
+})
+export class PaymentContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
